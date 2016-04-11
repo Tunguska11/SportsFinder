bad364f1e83e839de0e09eb3d184f63ec39b05d9
Brad <bradstell87@gmail.com>
2016-04-11 13:29:23 -0400

    Added SportEvent model class

---
+++ src/SportsFinder/Models/SportEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsFinder.Models
+{
+    public class SportEvent
+    {
+        public int ID { get; set; }
+        public DateTime EventTime { get; set; }
+        public string EventSport { get; set; }
+        public bool IsTentative { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public int PplAttendingCount { get; set; }
+        public int MaxPeopleAllowed { get; set; }
+        public string EquipmentList { get; set; }
+    }
+}
